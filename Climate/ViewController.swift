be6c65a69f7e968dd0ec02790dea9690de75fe6c
mactang <tang.1208@osu.edu>
2020-10-07 19:05:24 -0400

    Initial Commit

---
+++ Climate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Climate
+//
+//  Created by nachuan on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
